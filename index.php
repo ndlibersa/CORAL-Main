c115ffe8d630ea626d62efe77ced41169f183462
Ryan Chisum <ryan.chisum@sirsidynix.com>
2016-05-09 12:50:27 -0600

    Newly Designed Main Page

--- index.php
+++ index.php
@@ -28,36 +28,77 @@ for($i=0; $i<count($parts) - 1; $i++){
 	<div id="title-main"><img src='images/title-main.png' id="title-big"><img src='images/title-main-small.png' id="title-small"></div>
 
 		<div class="icons">
+		
+			<div class='main-page-icons'>
+				<?php if (file_exists($pagePath . "resources/index.php")) {?>
+					<a href='resources/'>
+						<img src='images/icon-resources.png' hover="images/icon-resources-hover.png" class="rollover" />
+						<span>Resources</span>
+					</a>
+				<?php } else { ?>
+					<div class='main-page-icons-off'>
+						<img src='images/icon-resources-off.png'>
+						<span>Resources</span>
+					</div>
+				<?php } ?>
+			</div>
 
-			<?php if (file_exists($pagePath . "resources/index.php")) {?>
-				<a href='resources/'><img src='images/icon-resources.png' hover="images/icon-resources-hover.png" class="rollover" /></a>
-			<?php } else { ?>
-				<img src='images/icon-resources-off.png'>
-			<?php } ?>
+		
+			<div class='main-page-icons'>
+				<?php if (file_exists($pagePath . "licensing/index.php")) {?>
+					<a href='licensing/'>
+						<img src='images/icon-licensing.png' hover="images/icon-licensing-hover.png" class="rollover" />
+						<span>Licensing</span>
+					</a>
+				<?php } else { ?>
+					<div class='main-page-icons-off'>
+						<img src='images/icon-licensing-off.png' />
+						<span>Licensing</span>
+					</div>
+				<?php } ?>
+			</div>
 
-			<?php if (file_exists($pagePath . "licensing/index.php")) {?>
-				<a href='licensing/'><img src='images/icon-licensing.png' hover="images/icon-licensing-hover.png" class="rollover" /></a>
-			<?php } else { ?>
-				<div><img src='images/icon-licensing-off.png' /></div>
-			<?php } ?>
+			<div class='main-page-icons'>
+				<?php if (file_exists($pagePath . "organizations/index.php")) {?>
+					<a href='organizations/'>
+						<img src='images/icon-organizations.png' hover="images/icon-organizations-hover.png" class="rollover" />
+						<span>Organizations</span>
+					</a>
+				<?php } else { ?>
+					<div class='main-page-icons-off'>
+						<img src='images/icon-organizations-off.png' />
+						<span>Organizations</span>
+					</div>
+				<?php } ?>
+			</div>
 
-			<?php if (file_exists($pagePath . "organizations/index.php")) {?>
-				<a href='organizations/'><img src='images/icon-organizations.png' hover="images/icon-organizations-hover.png" class="rollover" /></a>
-			<?php } else { ?>
-				<img src='images/icon-organizations-off.png' />
-			<?php } ?>
-
-			<?php if (file_exists($pagePath . "usage/index.php")) {?>
-				<a href='usage/'><img src='images/icon-usage.png' hover="images/icon-usage-hover.png" class="rollover" /></a>
-			<?php } else { ?>
-				<img src='images/icon-usage-off.png' />
-			<?php } ?>
-			
-			<?php if (file_exists($pagePath . "management/index.php")) {?>
-				<a href='management/'><img src='images/icon-management.png' hover="images/icon-management-hover.png" class="rollover" /></a>
-			<?php } else { ?>
-				<img src='images/icon-usage-off.png' />
-			<?php } ?>
+			<div class='main-page-icons'>
+				<?php if (file_exists($pagePath . "usage/index.php")) {?>
+					<a href='usage/'>
+						<img src='images/icon-usage.png' hover="images/icon-usage-hover.png" class="rollover" />
+						<span>Usage Statistics</span>
+					</a>	
+				<?php } else { ?>
+					<div class='main-page-icons-off'>
+						<img src='images/icon-usage-off.png' />
+						<span>Usage Statistics</span>
+					</div>
+				<?php } ?>
+			</div>
+		
+			<div class='main-page-icons'>
+				<?php if (file_exists($pagePath . "management/index.php")) {?>
+					<a href='management/'>
+						<img src='images/icon-management.png' hover="images/icon-management-hover.png" class="rollover" />
+						<span>Management</span>
+					</a>
+				<?php } else { ?>
+					<div class='main-page-icons-off'>
+						<img src='images/icon-management-off.png' />
+						<span>Management</span>
+					</div>
+				<?php } ?>
+			</div>
 
 		</div>
 		
